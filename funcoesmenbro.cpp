8cd43169f0417e0440cf417eb559ada0caaf4e41
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-19 08:52:53 -0300

    inicio da estrutura

--- funcoesmenbro.cpp
+++ funcoesmenbro.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct Pessoa {
+    char nome[50];
+    int idade;
+    float salario;
+
+    void exibirDados(){
+        cout << "Nome: " << nome << endl;
+        cout << "Idade: " << idade << endl;
+        cout << fixed << setprecision(4) << "Salario: " << salario << endl;// Use fixed e setprecision para formatar a saída
+    }
+};
+
 int main(){
     Pessoa pessoa;
 
