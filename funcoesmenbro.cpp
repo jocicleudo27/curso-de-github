827a0aa682a735ee2ba15ea7148394f71112cf68
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-19 15:07:17 -0300

    testando alterações

--- funcoesmenbro.cpp
+++ funcoesmenbro.cpp
@@ -10,7 +10,19 @@ struct Pessoa {
     void exibirDados(){
         cout << "Nome: " << nome << endl;
         cout << "Idade: " << idade << endl;
-        cout << fixed << setprecision(4) << "Salario: " << salario << endl;// Use fixed e setprecision para formatar a saída
+        cout << fixed << setprecision(4) << "Salario: " << salario << endl;
+    }
+
+    void atualizarDados(){
+        cout << "Digite o nome: ";
+        cin.getline(nome, 50);
+        cout << "Digite a idade: ";
+        cin >> idade;
+        cin.ignore();
+        cout << "Digite o salario: ";
+        cin.ignore(); //ignora o caracter de nova linha no buffer.
+        cin >> salario;
+        cin.ignore();
     }
 };
 
@@ -19,19 +31,22 @@ bool compararIdade(Pessoa p1, Pessoa p2){
 }
 
 int main(){
-    Pessoa pessoa1, pessoa2;
+   Pessoa pessoa1, pessoa2; //Cria dois objetos da estrutura Pessoa.
 
-    cout << "DIgite os dados da primeira pessoa:\n";
-    pessoa1.exibirDados();
+   //Preenche os dados da primeira pessoa.
+   cout << "Digite os dados da primeira pessoa:\n";
+   pessoa1.atualizarDados();
 
-    cout << "Digite os dados da segunda pessoa:\n";
-    pessoa2.exibirDados();
+   //Preenche os dados da segunda pessoa.
+   cout << "Digite os dados da segunda pessoa:\n";
+   pessoa2.atualizarDados();
 
-    //Comparar a idade das duas pessoas.
-    if(compararIdade(pessoa1, pessoa2)){
-        cout << "A primeira pessoa e mais velha.\n";
-    }else{
-        cout << "A segunda pessoa e mais velha.\n";
-    }
-    return 0;
+   // Compara a idade das duas pessoas.
+   if(compararIdade(pessoa1, pessoa2)){
+       cout << "A primeira pessoa e mais velha.\n";
+   } else {
+       cout << "A segunda pessoa e mais velha.\n";
+   }
+
+   return 0;
 }
